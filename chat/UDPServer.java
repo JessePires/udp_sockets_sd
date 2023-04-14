4e0a46ce57fb4d682a326016d0b1b5ab28197a57
jhonatancunha <jhonatancunha@alunos.utfpr.edu.br>
2023-04-13 23:03:20 -0300

    feat: criando listagem de emojis

--- chat/UDPServer.java
+++ chat/UDPServer.java
@@ -8,6 +8,8 @@ package chat;
 
 import java.net.*;
 import java.nio.ByteBuffer;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 import javax.swing.JOptionPane;
@@ -16,23 +18,7 @@ import java.io.*;
 
 public class UDPServer {
     public static void main(String args[]) {
-
-        // String nickname = "Jhonatan";
-        // String message = "oi!";
-        // byte messageType = 1;
-        // byte nicknameLength = (byte) nickname.length();
-        // byte messageLength = (byte) message.length();
-
-        // ByteBuffer header = createHeader(messageType, nicknameLength, nickname,
-        // messageLength, message);
-
-        // Obtem as informações do header que recebo do outro usuário
-        // String remoteUserNickName = getNickName(header);
-        // String remoteUserMessage = getMessage(header);
-        // int resp = 0;
-
         DatagramSocket dgramSocket = null;
-
         try {
 
             int srcPort = Integer.parseInt(JOptionPane.showInputDialog("Digite a porta de origem:"));
@@ -84,12 +70,33 @@ class SendDatagramThread extends Thread {
     String nickname;
     byte nicknameSize;
 
+    HashMap<Integer, String> emoji;
+
     public SendDatagramThread(DatagramSocket datagramSocket, InetAddress address, int port, String nickname) {
         this.datagramSocket = datagramSocket;
         this.address = address;
         this.port = port;
         this.nickname = nickname;
         this.nicknameSize = (byte) nickname.length();
+
+        // Criando emojis
+        this.emoji = new HashMap<>();
+        this.emoji.put(1, ":)");
+        this.emoji.put(2, ":(");
+        this.emoji.put(3, "xD");
+        this.emoji.put(4, ":*");
+    }
+
+    public void printEmojis() {
+
+        System.out.println("Escolha um dos emojis abaixo:");
+
+        for (Map.Entry<Integer, String> entrada : this.emoji.entrySet()) {
+            System.out.println(entrada);
+        }
+
+        System.out.print("Opção: ");
+
     }
 
     public ByteBuffer createHeader(
@@ -125,42 +132,85 @@ class SendDatagramThread extends Thread {
         return buffer;
     }
 
+    void printMessage(String message, byte messageType) {
+        switch (messageType) {
+            case 1:
+                System.out.println(ANSI_RED + "Você" + ": " + ANSI_RESET + "<Text> " + message);
+                break;
+            case 2:
+                System.out.println(ANSI_RED + "Você" + ": " + ANSI_RESET + "<Emoji> " + message);
+                break;
+            case 3:
+                System.out.println(ANSI_RED + "Você" + ": " + ANSI_RESET + "<URL> " + message);
+                break;
+            case 4:
+                System.out.println(ANSI_RED + "Você" + ": " + ANSI_RESET + "<ECHO> " + message);
+                break;
+        }
+    }
+
+    Map<Integer, String> getUserMessage() {
+        Map<Integer, String> message = new HashMap<>();
+
+        String messageType = "1";
+        String text = "";
+
+        Scanner reader = new Scanner(System.in);
+
+        text = reader.nextLine();
+
+        if (text.contains("emoji")) {
+            messageType = "2";
+            this.printEmojis();
+            Integer option = reader.nextInt();
+            text = this.emoji.get(option);
+        } else if (text.contains("url ")) {
+            messageType = "2";
+        } else if (text.contains("echo ")) {
+            messageType = "3";
+        }
+
+        message.put(0, messageType);
+        message.put(1, text);
+
+        return message;
+    }
+
     /* metodo executado ao iniciar a thread - start() */
     @Override
     public void run() {
-        int resp = 0;
-
-        Scanner reader = new Scanner(System.in);
-        String message = "";
         try {
+            String message = "";
             do {
-                // String message = JOptionPane.showInputDialog("Mensagem:");
-                System.out.print(ANSI_GREEN + "Mesagem: " + ANSI_RESET);
-                message = reader.nextLine();
+
+                Map<Integer, String> messageMap = getUserMessage();
+
+                message = messageMap.get(1);
+                byte messageType = (byte) Integer.parseInt(messageMap.get(0));
                 byte messageSize = (byte) message.length();
 
-                byte messageType = 1;
-                ByteBuffer header = this.createHeader(messageType, this.nicknameSize, this.nickname, messageSize,
+                ByteBuffer header = this.createHeader(
+                        messageType,
+                        this.nicknameSize,
+                        this.nickname,
+                        messageSize,
                         message);
+
                 byte[] headerByteArray = header.array();
 
                 // /* cria um pacote datagrama */
-                DatagramPacket request = new DatagramPacket(headerByteArray, headerByteArray.length,
+                DatagramPacket request = new DatagramPacket(
+                        headerByteArray,
+                        headerByteArray.length,
                         this.address,
                         this.port);
 
                 // /* envia o pacote */
                 this.datagramSocket.send(request);
-
-                System.out.println(ANSI_RED + this.nickname + ": " + ANSI_RESET + message);
-
-                // resp = JOptionPane.showConfirmDialog(null, "Nova mensagem?", "Continuar",
-                // JOptionPane.YES_NO_OPTION);
-                // } while (message != JOptionPane.NO_OPTION);
+                this.printMessage(message, messageType);
             } while (message != "exit");
         } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
-
         }
     }
 }
@@ -213,6 +263,27 @@ class ReceiveDatagramThread extends Thread {
         return message;
     }
 
+    public byte getMessageType(ByteBuffer header) {
+        return header.get(POS_MESSAGE_TYPE);
+    }
+
+    void printMessage(String message, byte messageType, String nickname) {
+        switch (messageType) {
+            case 1:
+                System.out.println(ANSI_RED + nickname + ": " + ANSI_RESET + "<Text>" + message);
+                break;
+            case 2:
+                System.out.println(ANSI_RED + nickname + ": " + ANSI_RESET + "<Emoji>" + message);
+                break;
+            case 3:
+                System.out.println(ANSI_RED + nickname + ": " + ANSI_RESET + "<URL>" + message);
+                break;
+            case 4:
+                System.out.println(ANSI_RED + nickname + ": " + ANSI_RESET + "<ECHO>" + message);
+                break;
+        }
+    }
+
     /* metodo executado ao iniciar a thread - start() */
     @Override
     public void run() {
@@ -229,10 +300,9 @@ class ReceiveDatagramThread extends Thread {
 
                 String nickname = getNickName(header);
                 String message = getMessage(header);
+                byte messageType = getMessageType(header);
 
-                System.out.println("\n" + ANSI_RED + nickname + ": " + ANSI_RESET + message);
-                System.out.print(ANSI_GREEN + "Mesagem: " + ANSI_RESET);
-
+                this.printMessage(message, messageType, nickname);
             }
         } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
